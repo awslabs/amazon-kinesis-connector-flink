1da6d52e36f39a860e3f5b89b8b552391793fb2c
Danny Cranmer <cranmerd@amazon.com>
2020-10-06 08:43:37 +0100

    Adding file modification header

--- src/test/java/software/amazon/kinesis/connectors/flink/metrics/ShardConsumerMetricsReporterTest.java
+++ src/test/java/software/amazon/kinesis/connectors/flink/metrics/ShardConsumerMetricsReporterTest.java
@@ -1,4 +1,6 @@
 /*
+ * This file has been modified from the original.
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
