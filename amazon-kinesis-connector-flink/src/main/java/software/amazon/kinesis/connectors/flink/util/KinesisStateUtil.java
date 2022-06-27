506e04f57394f01878fddebc9184771589adb86c
Alexander Egorov <alexego@amazon.com>
2022-06-27 12:22:55 -0500

    1. Add copyright header 2. Add private constructor to KinesisStateUtil to prevent instantiation

--- amazon-kinesis-connector-flink/src/main/java/software/amazon/kinesis/connectors/flink/util/KinesisStateUtil.java
+++ amazon-kinesis-connector-flink/src/main/java/software/amazon/kinesis/connectors/flink/util/KinesisStateUtil.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package software.amazon.kinesis.connectors.flink.util;
 
 import org.apache.flink.api.common.ExecutionConfig;
@@ -13,6 +29,12 @@ import software.amazon.kinesis.connectors.flink.model.StreamShardMetadata;
  * Utilities for Flink Kinesis connector state management.
  */
 public class KinesisStateUtil {
+
+    /**
+     * To prevent instantiation of class
+     */
+    private KinesisStateUtil() {}
+
     /**
      * Creates state serializer for kinesis shard sequence number.
      * Using of the explicit state serializer with KryoSerializer is needed because otherwise
